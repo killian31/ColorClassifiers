c97f69f4ac4cba13737041454b2d3c46f989cdb3
killian31 <killian.steunou@gmail.com>
2023-05-12 11:17:04 +0200

    feat: scripts for installing IB and SAM

---
+++ scripts/install_dep.sh
@@ -0,0 +1,3 @@
+git clone --recursive https://github.com/facebookresearch/ImageBind.git
+cd ImageBind; pip install -r requirements.txt
+pip install git+https://github.com/facebookresearch/segment-anything.git
