03d1e7d5032c4aab575f9d04f6f907af22a7f43a
killian31 <killian.steunou@gmail.com>
2023-05-16 17:26:45 +0200

    feat: benchmark on IB precision for colors

--- embeddings.py
+++ embeddings.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 import wget
 from segment_anything import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
+from tqdm import tqdm
 
 import data
 from models import imagebind_model
@@ -75,27 +76,73 @@ def get_masks(image_path, sam_checkpoint="sam_vit_b_01ec64.pth"):
     return sorted_masks, masked_images
 
 
-def get_embeddings(texts: [str], mask_paths: [str]):
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+def get_embeddings(filepath="./labels.txt", device="cuda:1"):
+    colors = ["white", "yellow", "red", "blue", "green"]
+    texts = [f"A {color} traffic sign." for color in colors]
     text_list = texts
+    lines = open(filepath, "r").read().splitlines()
+    success_count = 0
+    approx_success_count = 0
+    total = len(lines)
+    print("Loading model...")
     model = imagebind_model.imagebind_huge(pretrained=True)
     model.eval()
     model.to(device)
-    inputs = {
-        ModalityType.TEXT: data.load_and_transform_text(text_list, device),
-        ModalityType.VISION: data.load_and_transform_vision_data(mask_paths, device),
-    }
-    with torch.no_grad():
-        embeddings = model(inputs)
+    os.makedirs("./crops", exist_ok=True)
+    wrongs = []
+    with tqdm(range(total)) as pbar:
+        for i in range(total):
+            img_path, truth = lines[i].split(" ")
+            mask_path = img_path[:-4] + "_mask.png"
+            mask = cv2.imread(mask_path, 0)
+            x, y, w, h = cv2.boundingRect(mask)
+            img = cv2.imread(img_path)
+            crop = img[y : y + h, x : x + w, :]
+            crop_filename = f"crops/{os.path.basename(img_path)}.jpg"
+            cv2.imwrite(crop_filename, crop)
+            inputs = {
+                ModalityType.TEXT: data.load_and_transform_text(text_list, device),
+                ModalityType.VISION: data.load_and_transform_vision_data(
+                    [crop_filename], device
+                ),
+            }
+            with torch.no_grad():
+                embeddings = model(inputs)
 
-    probs = torch.softmax(
-        embeddings[ModalityType.TEXT] @ embeddings[ModalityType.VISION].T, dim=-1
-    )
-    print(
-        "Text x Vision: ",
-        probs,
-    )
+            probs = torch.softmax(
+                embeddings[ModalityType.VISION] @ embeddings[ModalityType.TEXT].T,
+                dim=-1,
+            )
+            output = probs[0].cpu().numpy()
+            best = np.argmax(
+                output,
+                axis=0,
+            )
+            approx_colors = [colors[i] for i in range(len(output)) if output[i] > 0.15]
+            predicted_color = colors[best]
+            if predicted_color == truth:
+                success_count += 1
+                approx_success_count += 1
+                pbar.set_description("Got it right.")
+            elif predicted_color in approx_colors:
+                approx_success_count += 1
+            else:
+                pbar.set_description("Got it wrong.")
+                wrongs.append(
+                    {
+                        "image_path": crop_filename,
+                        "predicted": predicted_color,
+                        "real_color": truth,
+                        "raw_output": output,
+                    }
+                )
+            pbar.update(1)
+    print("Real success", success_count / total)
+    print("Approximate success:", approx_success_count / total)
+    with open("wrongs.txt", "w") as f:
+        f.write(wrongs)
 
+    """
     output = probs[0].cpu().numpy()
     best = np.argmax(
         output,
@@ -105,25 +152,36 @@ def get_embeddings(texts: [str], mask_paths: [str]):
         if output[i] > 0.15:
             print(mask_paths[i])
     print(f"Best mask: {mask_paths[best]}")
+    """
 
 
 if __name__ == "__main__":
-    if not os.path.exists("./masks/"):
-        is_masks = False
-        os.makedirs("./masks", exist_ok=True)
-    else:
-        is_masks = True
-    if not is_masks:
-        masks, masked_images = get_masks(image_path=".assets/car_image.jpg")
-        mask_filenames = []
-        for i, masked_image in enumerate(masked_images):
-            filename = f"./masks/mask_{i+1:03d}.jpg"
-            cv2.imwrite(filename, masked_image)
-            mask_filenames.append(filename)
-    else:
-        mask_filenames = ["./masks/" + f for f in os.listdir("./masks/")]
-    print(f"Using {len(os.listdir('./masks/'))} masks.")
+    """
+    use_masks = False
 
-    mask_paths = sorted(mask_filenames)
+    if use_masks:
+        if not os.path.exists("./masks/"):
+            is_masks = False
+            os.makedirs("./masks", exist_ok=True)
+        else:
+            is_masks = True
+        if not is_masks:
+            masks, masked_images = get_masks(image_path=".assets/car_image.jpg")
+            mask_filenames = []
+            for i, masked_image in enumerate(masked_images):
+                filename = f"./masks/mask_{i+1:03d}.jpg"
+                cv2.imwrite(filename, masked_image)
+                mask_filenames.append(filename)
+        else:
+            mask_filenames = ["./masks/" + f for f in os.listdir("./masks/")]
+        print(f"Using {len(os.listdir('./masks/'))} masks.")
 
-    get_embeddings(["A car."], mask_paths)
+        mask_paths = sorted(mask_filenames)
+
+        images_paths = mask_paths
+    else:
+        images_paths = ["./images/" + path for path in sorted(os.listdir("./images/"))]
+    """
+
+    device = "cuda:1" if torch.cuda.is_available() else "cpu"
+    get_embeddings(device=device)
